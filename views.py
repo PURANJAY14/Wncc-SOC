1617f2fd876b665b734e2a59676c54c562f03730
PURANJAY14 <62548743+PURANJAY14@users.noreply.github.com>
2020-04-27 22:57:16 +0530

    Delete views.py

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Destination
-# Create your views here.
-def index(request):
-   dest1=Destination.objects.all()
-   return render(request,"index.html",{'dest1':dest1})
